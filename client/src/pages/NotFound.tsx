d7e1599a5e892309d50aef8b2ec57e00261caafd
Rostyslav <hasiukrostyslav@gmail.com>
2024-02-25 16:19:42 +0200

    Added 404 error img

--- client/src/pages/NotFound.tsx
+++ client/src/pages/NotFound.tsx
@@ -3,6 +3,7 @@ import ButtonLink from '../components/ButtonLink';
 function NotFound() {
   return (
     <div className="flex flex-col items-center justify-center gap-12">
+      <img className="flex w-52" src="/error-404.png" alt="" />
       <div className="flex items-center gap-4 text-slate-950">
         <span className="text-4xl font-semibold">404</span>
         <span className="text-5xl">|</span>
