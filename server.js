9afed12ce44f17327657cc60c17576bd8a0520c2
Rostyslav <hasiukrostyslav@gmail.com>
2024-02-25 14:13:18 +0200

    Connected MongoDB to server

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
+const mongoose = require('mongoose');
+
 const app = require('./app');
+
+mongoose
+  .connect(process.env.DATABASE_URI)
+  .then(() => console.log('Database is connected'));
 
 const port = process.env.PORT || 3000;
 
