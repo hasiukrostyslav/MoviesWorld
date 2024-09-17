51e015f709ae8f4d774c37c529134cb7b5b8764a
Rostyslav <hasiukrostyslav@gmail.com>
2024-09-17 17:53:44 +0300

    Add scroll to top functionality

--- client/src/pages/Layout.tsx
+++ client/src/pages/Layout.tsx
@@ -1,16 +1,20 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import ScrollToTop from '../components/ScrollToTop';
 
 function Layout() {
   return (
-    <section className="">
-      <div className="relative px-40 pt-10 3xl:px-72">
-        <Navbar />
-        <Outlet />
-      </div>
-      <Footer />
-    </section>
+    <>
+      <ScrollToTop />
+      <section className="">
+        <div className="relative px-40 pt-10 3xl:px-72">
+          <Navbar />
+          <Outlet />
+        </div>
+        <Footer />
+      </section>
+    </>
   );
 }
 
