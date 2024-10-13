d9d90a327ec74f56b1b46d322911d82e34ba32f0
Rostyslav <hasiukrostyslav@gmail.com>
2024-10-13 14:20:10 +0300

    Made Navbar fixed

--- client/src/hooks/useMatchTheme.tsx
+++ client/src/hooks/useMatchTheme.tsx
@@ -1,6 +1,8 @@
 import { useLocation, useMatch } from 'react-router-dom';
+import { useScroll } from './useScroll';
 
 export function useMatchTheme() {
+  const { isScroll } = useScroll();
   const matchHome = useMatch('/');
   const matchCollection = useMatch('collections');
   const location = useLocation();
@@ -8,7 +10,7 @@ export function useMatchTheme() {
     location.pathname.includes('view/tv') ||
     location.pathname.includes('view/movie');
 
-  const match = matchHome || matchCollection || matchPath;
+  const match = (matchHome || matchCollection || matchPath) && !isScroll;
 
   return match;
 }
--- client/src/pages/Layout.tsx
+++ client/src/pages/Layout.tsx
@@ -2,12 +2,21 @@ import { Outlet } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import ScrollToTop from '../components/ScrollToTop';
+import { useScroll } from '../hooks/useScroll';
+import { createPortal } from 'react-dom';
 
 function Layout() {
+  const { isScroll } = useScroll();
+
   return (
     <>
       <ScrollToTop />
-      <section className="">
+      {isScroll &&
+        createPortal(
+          <div className="fixed left-0 top-0 z-40 h-24 w-screen bg-slate-200  dark:bg-slate-800"></div>,
+          document.body,
+        )}
+      <section>
         <div className="relative px-40 pt-10 3xl:px-72">
           <Navbar />
           <Outlet />
